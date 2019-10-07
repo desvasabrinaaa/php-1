043e4b1f2531e79e890abc18bca46300a22910f8
desvasabrinaaa <53281319+desvasabrinaaa@users.noreply.github.com>
2019-10-07 20:12:10 +0700

    Create belajar.php

---
+++ belajar.php
@@ -0,0 +1,11 @@
+<?php
+echo "Hello World !"; /*untuk menampilkan sebuah string*/
+echo "<h1>Hello World !</h1>"; /*untuk menampilkan sebuah string*/
+// VARIABEL
+// diawali dengan tanda $, ex = $nama 
+// nama variabel tidak boleh diawali dengan angka, ex = $11nama 
+// boleh mengandung angka
+$nama = "Desva Sabrina";
+$pesan = "Halo, Selamat Datang <strong>$nama</strong>";
+echo $pesan;
+?>
